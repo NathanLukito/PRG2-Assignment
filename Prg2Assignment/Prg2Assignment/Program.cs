3664d23756ee40bc63141bd5bf0c638acf4ad9c5
ErvinW <116048866+ErvinW@users.noreply.github.com>
2023-01-16 21:47:49 +0800

    Working on function ShowGuestDetails

--- Prg2Assignment/Prg2Assignment/Program.cs
+++ Prg2Assignment/Prg2Assignment/Program.cs
@@ -1,1 +1,9 @@
 ﻿
+string GuestPath = "https://github.com/NathanLukito/PRG2-Assignment/blob/1aa849a8de2b114df39984bad9635403d24ab44b/Prg2Assignment/Prg2Assignment/bin/Debug/net6.0/Guests.csv";
+string RoomsPath = "https://github.com/NathanLukito/PRG2-Assignment/blob/1aa849a8de2b114df39984bad9635403d24ab44b/Prg2Assignment/Prg2Assignment/bin/Debug/net6.0/Rooms.csv";
+string StaysPath = "https://github.com/NathanLukito/PRG2-Assignment/blob/1aa849a8de2b114df39984bad9635403d24ab44b/Prg2Assignment/Prg2Assignment/bin/Debug/net6.0/Stays.csv";
+//Display all guest details//
+void ShowGuestDetails(){
+    string[] GuestDetails = File.ReadAllLines(GuestPath);
+
+}
