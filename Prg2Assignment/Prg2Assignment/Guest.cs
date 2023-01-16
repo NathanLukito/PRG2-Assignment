29615dfef872cc1431edf33a6b10cae819e170e3
ErvinW <116048866+ErvinW@users.noreply.github.com>
2023-01-16 13:22:50 +0800

    Add classes membership, Guest and Stay

--- Prg2Assignment/Prg2Assignment/Guest.cs
+++ Prg2Assignment/Prg2Assignment/Guest.cs
@@ -8,5 +8,29 @@ namespace Prg2Assignment
 {
     class Guest
     {
+        public string name { get; set; }
+        public string passportNum { get; set; }
+
+        public Stay hotelStay { get; set; }
+        
+        public Membership membership { get; set; }
+
+        public bool iSCheckedin { get; set; }
+
+        //Constructor
+        public Guest() { }
+
+        public Guest(string n, string pass, Stay hs, Membership mem)
+        {
+            name = n;
+            passportNum = pass;
+            hotelStay = hs;
+            membership = mem;
+        } 
+
+        public override string ToString()
+        {
+            return "Name: " + name + "\tPassport no: " + passportNum + "\tMembership: " + membership.ToString() + "Hotel stay: " + hotelStay.ToString();
+        }
     }
 }
--- Prg2Assignment/Prg2Assignment/Stay.cs
+++ Prg2Assignment/Prg2Assignment/Stay.cs
@@ -8,5 +8,26 @@ namespace Prg2Assignment
 {
     class Stay
     {
+        public DateTime checkinDate { get; set; }
+        public DateTime checkoutDate { get; set;}
+
+        public List<Room> roomlist { get; set; }
+
+        //Constructors
+        public Stay() { }
+
+        public Stay (DateTime cid, DateTime cod)
+        {
+            checkinDate= cid;
+            checkoutDate= cod;
+        }
+
+
+        //functions
+        public override string ToString()
+        {
+            return base.ToString() + "\tCheck in date: " + checkinDate + "\tCheck out date: " + checkoutDate;
+        }
+
     }
 }
